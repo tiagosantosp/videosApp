1086e1f013361dad636bdf36bb69797af08214e8
tiagosantosp <tiago_s.p@hotmail.com>
2021-12-09 15:25:33 -0300

    Adicionamos o array de filmes

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,8 @@
+import { IFilmes } from '../models/IFilme.model';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-tab1',
@@ -8,7 +11,28 @@ import { AlertController } from '@ionic/angular';
 })
 export class Tab1Page {
 
-  constructor(public alertController: AlertController) {}
+  titulo = 'Videos App';
+
+  listaVideos: IFilmes[] =[
+    {
+      nome: 'The Flash (2014)',
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/lJA2RCMfsWoskqlQhXPSLFQGXEJ.jpg',
+      classificacao: 78,
+      duracao: '44 min',
+      generos: ['Drama', 'Sci-Fi', 'Fantasy'],
+      lancamento: '07/10/2021'
+    },
+    {
+      nome: 'Venom: Tempo de Carnificina (2021)',
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/h5UzYZquMwO9FVn15R2eK2itmHu.jpg',
+      classificacao: 72,
+      duracao: '1h 37min',
+      generos: ['Ficção científica', 'Ação', 'Aventura'],
+      lancamento: '07/10/2021'
+    },
+  ];
+
+  constructor(public alertController: AlertController, public toastController: ToastController) {}
 
 
   async exibirAlertaFavorito() {
@@ -27,13 +51,21 @@ export class Tab1Page {
         },
         {
           text: 'Sim',
-          handler: () => {
-            console.log('Confirm Okay');
-          },
+          handler: () => this.presentToast()
+          ,
         },
       ],
     });
 
     await alert.present();
   }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'filme favoritado!',
+      duration: 2000,
+    });
+    toast.present();
+  }
+
 }
